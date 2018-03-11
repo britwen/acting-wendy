c0519023c21a7c61f948cc29261e83df21c377aa
Christian Knauer <christiancalled@gmail.com>
2018-03-11 00:14:21 -0800

    added underline for nav on landing on hover. also added jquery for nav-marker toggle on landing.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+﻿var nav__markerLanding = $(".nav__markerLanding");
+
+$(".nav__a").hover(function () {
+    $(nav__markerLanding).hide();
+});
+
+//if ($(".nav__markerLanding").is(":hidden")) {
+
+//    alert('test');
+
+//}
+
+$('.nav__ul').mouseleave(function () {
+    $(nav__markerLanding).show();
+})
