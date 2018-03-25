e94d7df12c9ade6d306fee781040d56524478775
Christian Knauer <christiancalled@gmail.com>
2018-03-25 00:21:33 -0700

    starting implementation for hover effect on gallery page.

--- js/script.js
+++ js/script.js
@@ -8,3 +8,42 @@ $('.navLanding__a').hover(function () {
 $('.navLanding__ul').mouseleave(function () {
     $(navLanding__markerLanding).show();
 })
+
+
+
+var imgBox = document.getElementsByClassName("imgBox");
+imgBox = imgBox[0];
+
+
+var col1 = document.getElementsByClassName("col1");
+col1 = col1[0];
+var col2 = document.getElementsByClassName("col2");
+col2 = col2[0];
+var col3 = document.getElementsByClassName("col3");
+col3 = col3[0];
+var col4 = document.getElementsByClassName("col4");
+col4 = col4[0];
+var col5 = document.getElementsByClassName("col5");
+col5 = col5[0];
+
+
+col1.addEventListener("mouseenter", function () {
+    imgBox.style.background = "url('/img/wendy_about.jpg')";
+    imgBox.style.backgroundSize = "cover";
+});
+col2.addEventListener("mouseenter", function () {
+    imgBox.style.background = "url('/img/wendy_landing.png')";
+    imgBox.style.backgroundSize = "cover";
+});
+col3.addEventListener("mouseenter", function () {
+    imgBox.style.background = "url('/img/wendy_about.jpg')";
+    imgBox.style.backgroundSize = "cover";
+});
+col4.addEventListener("mouseenter", function () {
+    imgBox.style.background = "url('/img/wendy_landing.png')";
+    imgBox.style.backgroundSize = "cover";
+});
+col5.addEventListener("mouseenter", function () {
+    imgBox.style.background = "url('/img/wendy_about.jpg')";
+    imgBox.style.backgroundSize = "cover";
+});
