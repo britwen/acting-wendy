d75fc1f3d6193c9e0afe69cea0ab7ecd0f4e499c
Christian Knauer <christiancalled@gmail.com>
2018-11-07 20:25:13 +0100

    refactor img-hover-section on gallery page

--- js/script.js
+++ js/script.js
@@ -12,125 +12,44 @@ $('.navLanding__ul').mouseleave(function () {
 
 // FOLLOWING SECTION: IMG ANIMATION ON GALLERY PAGE
 
-// creates an array with the three image boxes
-var imgBox = document.getElementsByClassName("imgBox");
-// creates five arrays with each three columns
-var col1 = document.getElementsByClassName("col1");
-var col2 = document.getElementsByClassName("col2");
-var col3 = document.getElementsByClassName("col3");
-var col4 = document.getElementsByClassName("col4");
-var col5 = document.getElementsByClassName("col5");
-
-// START 1ST IMG-BOX
-
-// assigns the first image box to its var
-var imgBox1 = imgBox[0];
-// assigns the columns of the first image-box to its vars
-var imgBox1__col1 = col1[0];
-var imgBox1__col2 = col2[0];
-var imgBox1__col3 = col3[0];
-var imgBox1__col4 = col4[0];
-var imgBox1__col5 = col5[0];
-
-// if user hovers over first column of the first image box display img
-imgBox1__col1.addEventListener("mouseenter", function () {
-    imgBox1.style.background = "url('img/gangster_thumb.jpg')";
-    imgBox1.style.backgroundSize = "cover";
-});
-// if user hovers over second column of the first image box display img
-imgBox1__col2.addEventListener("mouseenter", function () {
-    imgBox1.style.background = "url('img/business_thumb.jpg')";
-    imgBox1.style.backgroundSize = "cover";
-});
-// if user hovers over 3rd column of the first image box display im
-imgBox1__col3.addEventListener("mouseenter", function () {
-    imgBox1.style.background = "url('img/retro_thumb.jpg')";
-    imgBox1.style.backgroundSize = "cover";
-});
-// if user hovers over 4th column of the first image box display im
-imgBox1__col4.addEventListener("mouseenter", function () {
-    imgBox1.style.background = "url('img/hijab_thumb.jpg')";
-    imgBox1.style.backgroundSize = "cover";
-});
-// if user hovers over 5th column of the first image box display im
-imgBox1__col5.addEventListener("mouseenter", function () {
-    imgBox1.style.background = "url('img/theat_thumb.jpg')";
-    imgBox1.style.backgroundSize = "cover";
-});
-
-// START 2ND IMG-BOX
-
-// assigns the 2nd image box to its var
-var imgBox2 = imgBox[1];
-// assigns the columns of the 2nd image-box to its vars
-var imgBox2__col1 = col1[1];
-var imgBox2__col2 = col2[1];
-var imgBox2__col3 = col3[1];
-var imgBox2__col4 = col4[1];
-var imgBox2__col5 = col5[1];
-
-// if user hovers over first column of the 2nd image box display img
-imgBox2__col1.addEventListener("mouseenter", function () {
-    imgBox2.style.background = "url('img/1com_thumb.jpg')";
-    imgBox2.style.backgroundSize = "cover";
-});
-// if user hovers over second column of the 2nd image box display img
-imgBox2__col2.addEventListener("mouseenter", function () {
-    imgBox2.style.background = "url('img/2com_thumb.jpg')";
-    imgBox2.style.backgroundSize = "cover";
-});
-// if user hovers over 3rd column of the 2nd image box display im
-imgBox2__col3.addEventListener("mouseenter", function () {
-    imgBox2.style.background = "url('img/3com_thumb.jpg')";
-    imgBox2.style.backgroundSize = "cover";
-});
-// if user hovers over 4th column of the 2nd image box display im
-imgBox2__col4.addEventListener("mouseenter", function () {
-    imgBox2.style.background = "url('img/4com_thumb.jpg')";
-    imgBox2.style.backgroundSize = "cover";
-});
-// if user hovers over 5th column of the 2nd image box display im
-imgBox2__col5.addEventListener("mouseenter", function () {
-    imgBox2.style.background = "url('img/4com_thumb.jpg')";
-    imgBox2.style.backgroundSize = "cover";
-});
-
-// START 3RD IMG-BOX
-
-// assigns the 3rd image box to its var
-var imgBox3 = imgBox[2];
-// assigns the columns of the 3rd image-box to its vars
-var imgBox3__col1 = col1[2];
-var imgBox3__col2 = col2[2];
-var imgBox3__col3 = col3[2];
-var imgBox3__col4 = col4[2];
-var imgBox3__col5 = col5[2];
-
-// if user hovers over first column of the first image box display img
-imgBox3__col1.addEventListener("mouseenter", function () {
-    imgBox3.style.background = "url('img/kettle_thumb.jpg')";
-    imgBox3.style.backgroundSize = "cover";
-});
-// if user hovers over second column of the first image box display img
-imgBox3__col2.addEventListener("mouseenter", function () {
-    imgBox3.style.background = "url('img/glove_prep_thumb.jpg')";
-    imgBox3.style.backgroundSize = "cover";
-});
-// if user hovers over 3rd column of the first image box display im
-imgBox3__col3.addEventListener("mouseenter", function () {
-    imgBox3.style.background = "url('img/jump_rope_thumb.jpg')";
-    imgBox3.style.backgroundSize = "cover";
-});
-// if user hovers over 4th column of the first image box display im
-imgBox3__col4.addEventListener("mouseenter", function () {
-    imgBox3.style.background = "url('img/boxing_bag_thumb.jpg')";
-    imgBox3.style.backgroundSize = "cover";
-});
-// if user hovers over 5th column of the first image box display im
-imgBox3__col5.addEventListener("mouseenter", function () {
-    imgBox3.style.background = "url('img/sparring_thumb.jpg')";
-    imgBox3.style.backgroundSize = "cover";
-});
+// array that holds images 
+var boxImages = [
+"url('img/gangster_thumb.jpg')",
+"url('img/business_thumb.jpg')",
+"url('img/retro_thumb.jpg')",
+"url('img/hijab_thumb.jpg')",
+"url('img/theat_thumb.jpg')",
+"url('img/1com_thumb.jpg')",
+"url('img/2com_thumb.jpg')",
+"url('img/3com_thumb.jpg')",
+"url('img/4com_thumb.jpg')",
+"url('img/4com_thumb.jpg')",
+"url('img/kettle_thumb.jpg')",
+"url('img/glove_prep_thumb.jpg')",
+"url('img/jump_rope_thumb.jpg')",
+"url('img/boxing_bag_thumb.jpg')",
+"url('img/sparring_thumb.jpg')"
+];
+// function that makes use of the event-object, so we can delegate from containing element to element where event occurred 
+function changeBgImg(e) {
+    // assign event-target
+    var target = e.target;
+    // loop through number of columns
+    for (i = 0; i < document.getElementsByClassName('col').length; i++) {
+        // if the current target is a column
+        if (target == document.getElementsByClassName('col')[i]) {
+            // change the columns parent (one of the three imgBox) background to the matching img from the boxImages-array
+            target.parentElement.style.background = boxImages[i];
+            target.parentElement.style.backgroundSize = "cover";
+        }
+    }
+    // event shouldn't bubble up to containing elements
+    e.stopPropagation();
+}
+// get containing element
+var imgBoxWrapper = document.getElementsByClassName('imgBoxWrapper')[0];
+// attach event-listener to containing element that triggers function on mouse-over
+imgBoxWrapper.addEventListener('mouseover', changeBgImg, false);
 
 
 
@@ -154,6 +73,9 @@ function gallery2() {
 function gallery3() {
     document.getElementById('project-load').innerHTML = '<object id="object3" type="text/html" data="gallery3.html"></object>';
 }
+
+// get elements
+var imgBox = document.getElementsByClassName('imgBox');
 // gallery-functions are called by onclick-eventHandler which is attached to imgBox-element
 imgBox[0].onclick = gallery1;
 imgBox[1].onclick = gallery2;
